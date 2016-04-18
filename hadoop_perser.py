a72085b7e8809e1ace08d601af9bc0ae7f914905
joyrahman <joyrahman@gmail.com>
2016-04-18 15:52:32 -0500

    04-18-2016

--- hadoop_perser.py
+++ hadoop_perser.py
@@ -34,6 +34,8 @@ def export_to_csv(data,file_name):
     output_file = output_file + file_extension
     header = "container_id,attempt_id,container_no,creation_time,start_time,end_time,node,status\n"
     output_file_loc = os.path.normpath(os.path.join(output_dir,output_file))
+    print "****************************"
+    print "output hadoop job details:{}".format(output_file_loc)
     with open(output_file_loc,'w') as f:
         f.write(header)
         for k, v in data.items():
