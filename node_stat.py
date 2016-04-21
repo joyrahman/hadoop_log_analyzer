d1b48d75863bdaab614e56bbbeecffefcd04a89b
joyrahman <joyrahman@gmail.com>
2016-04-21 12:52:21 -0500

    04-21-2016

--- node_stat.py
+++ node_stat.py
@@ -1,13 +1,22 @@
 import sys
+result={}
 def main(file_name):
 
     with open (file_name,'r') as f:
         for line in f:
             data = line.split('@')
             #print data
-            print data[0],data[6], data[10]
-               #data[10].rstrip('\n')
+            print data[0],data[6], data[10].rstrip('\n')
+            key = data[6]
+            value = data[10]
+            if key in result.keys():
+                if float(value)>result[key]:
+                    result[key]=value
+            else:
+                result[key]=value
+
 
 
 if __name__=="__main__":
     main(sys.argv[1])
+    print(result)
