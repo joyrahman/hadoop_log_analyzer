7e3227b5ff7fafda82aa0aeb7088daba0d7f0ef3
joyrahman <joyrahman@gmail.com>
2016-04-21 12:43:46 -0500

    04-21-2016

--- node_stat.py
+++ node_stat.py
@@ -4,8 +4,8 @@ def main(file_name):
     with open (file_name,'r') as f:
         for line in f:
             data = line.split('@')
-            print data
-            #print data[0],data[6]
+            #print data
+            print data[0],data[6],data[10].rstrip('\n')
 
 
 if __name__=="__main__":
