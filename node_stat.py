253a762a9ab9d9fbffc22ab4bcebc6654d25631e
joyrahman <joyrahman@gmail.com>
2016-04-21 12:59:58 -0500

    04-21-2016

--- node_stat.py
+++ node_stat.py
@@ -5,8 +5,11 @@ def main(file_name):
     with open (file_name,'r') as f:
         for line in f:
             data = line.split('@')
+            if (data[10]=='duration'):
+                pass
             #print data
             print data[0],data[6], data[10].rstrip('\n')
+
             key = data[6]
             value = float(data[10])
             if key in result.keys():
