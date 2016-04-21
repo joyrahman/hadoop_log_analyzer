692e2d6ed4f0bc64bf93d6f60b869a3b3738bd32
joyrahman <joyrahman@gmail.com>
2016-04-21 14:42:18 -0500

    04-21-2016

--- node_stat.py
+++ node_stat.py
@@ -100,7 +100,7 @@ if __name__=="__main__":
 
         print file_name
         #os.path.join(__file__,'data',file_name)
-        if "object" and "csv" in file_name:
+        if "object" in file_name:
             build_iostat_data(file_name)
         elif "csv" in file_name:
             build_hadoop_data(file_name)
