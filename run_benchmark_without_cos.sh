7f80fc8bd79833d84ddfae0197b8f239fc1dc845
joyrahman <joyrahman@gmail.com>
2016-05-13 10:32:03 -0500

    05-13-2016

--- run_benchmark_without_cos.sh
+++ run_benchmark_without_cos.sh
@@ -2,27 +2,21 @@ source ~/admin-openrc.sh
 swift delete result
 cd ~/hadoop_log_analyzer
 ssh palden@hyper 'source clear_cache/clean.sh'
-source benchmark_sort_cos.sh 0513201641 4M
-sleep 300
+source benchmark_sort.sh 0513201644 4M
+sleep 150
 ssh palden@hyper 'source clear_cache/clean.sh'
-source benchmark_grep_cos.sh 0513201642 4M
-sleep 300
+source benchmark_grep.sh 0513201645 4M
+sleep 150
 ssh palden@hyper 'source clear_cache/clean.sh'
-source benchmark_wordcount_cos.sh 0513201643 4M
-sleep 300
+source benchmark_wordcount.sh 0513201646 4M
+sleep 150
+
 swift delete result
 ssh palden@hyper 'source clear_cache/clean.sh'
-source benchmark_sort_cos.sh 05132016161 16M
-sleep 300
+source benchmark_sort.sh 05132016164 16M
+sleep 150
 ssh palden@hyper 'source clear_cache/clean.sh'
-source benchmark_grep_cos.sh 05132016162 16M
-sleep 300
+source benchmark_grep.sh 05132016165 16M
+sleep 150
 ssh palden@hyper 'source clear_cache/clean.sh'
-source benchmark_wordcount_cos.sh 05132016163 16M
-sleep 300
-
-
-
-
-
-
+source benchmark_wordcount.sh 05132016166 16M
