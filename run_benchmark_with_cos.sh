b963d9c37030d53da8670c45baaf77c373db4bd6
joyrahman <joyrahman@gmail.com>
2016-05-13 10:08:33 -0500

    05-13-2016

--- run_benchmark_with_cos.sh
+++ run_benchmark_with_cos.sh
@@ -1,18 +1,18 @@
 source ~/admin-openrc.sh
 swift delete result
 cd ~/hadoop_log_analyzer
-source benchmark_sort_cos.sh 0505201541 4M
+source benchmark_sort_cos.sh 0513201641 4M
 sleep 300
-source benchmark_grep_cos.sh 0505201542 4M
+source benchmark_grep_cos.sh 0513201642 4M
 sleep 300
-source benchmark_wordcount_cos.sh 0505201543 4M
+source benchmark_wordcount_cos.sh 0513201643 4M
 sleep 300
 swift delete result
-source benchmark_sort_cos.sh 05052015161 16M
+source benchmark_sort_cos.sh 05132016161 16M
 sleep 300
-source benchmark_grep_cos.sh 05052015162 16M
+source benchmark_grep_cos.sh 05132016162 16M
 sleep 300
-source benchmark_wordcount_cos.sh 05052015163 16M
+source benchmark_wordcount_cos.sh 05132016163 16M
 sleep 300
 
 
@@ -20,17 +20,17 @@ sleep 300
 source ~/admin-openrc.sh
 swift delete result
 cd ~/hadoop_log_analyzer
-source benchmark_sort.sh 0505201544 4M
+source benchmark_sort.sh 0513201644 4M
 sleep 300
-source benchmark_grep.sh 0505201545 4M
+source benchmark_grep.sh 0513201645 4M
 sleep 300
-source benchmark_wordcount.sh 0505201546 4M
+source benchmark_wordcount.sh 0513201646 4M
 sleep 300
 swift delete result
-source benchmark_sort.sh 05052015164 16M
+source benchmark_sort.sh 05132016164 16M
 sleep 300
-source benchmark_grep.sh 05052015165 16M
+source benchmark_grep.sh 05132016165 16M
 sleep 300
-source benchmark_wordcount.sh 05052015166 16M
+source benchmark_wordcount.sh 05132016166 16M
 
 
