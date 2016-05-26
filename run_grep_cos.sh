f35c71e9142303a409848113136edce7a3d6bce8
joyrahman <joyrahman@gmail.com>
2016-05-26 15:23:07 -0500

    05-26-2016

---
+++ run_grep_cos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source ~/admin-openrc.sh
+swift delete result
+cd ~/hadoop_log_analyzer
+prefix=`date +%m%d%Y%M%H`
+id='grep_cos'
+ssh palden@hyper 'source clear_cache/clean.sh'
+source benchmark_grep_cos.sh ${prefix}${id} 4M
