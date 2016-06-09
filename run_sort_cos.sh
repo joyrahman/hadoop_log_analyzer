13328a64a89605a53811a89ae87279a5d8c24f7c
joyrahman <joyrahman@gmail.com>
2016-06-09 12:41:47 -0500

    06-09-2016

--- run_sort_cos.sh
+++ run_sort_cos.sh
@@ -5,4 +5,4 @@ cd ~/hadoop_log_analyzer
 prefix=`date +%m%d%Y%M%H`
 id='sort_cos'
 ssh palden@hyper 'source clear_cache/clean.sh'
-source benchmark_sort_cos.sh ${prefix}${id} 4M
+source benchmark_sort_cos.sh ${prefix}${id} 16M
